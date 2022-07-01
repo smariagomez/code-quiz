36426a2e63cbc1f2e52b65bfd3b4874949266bd7
Stephanie Gomez <stephaniemariagomez@gmail.com>
2022-06-30 17:35:49 -0700

    adjusted css added google font, completed clear interval related to final question

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ var buttonFour = document.querySelector("#option-four");
 var card = document.querySelector("#cardDiv");
 var answerResponse = document.getElementById("answer");
 var index = 0;
+var scoreReader = document.getElementById("score-count")
+
 //questions array
 var questions = [
     {
@@ -46,89 +48,88 @@ var questions = [
 ]
 
 //console log examples console
-//log(questions[0].answers[2])
-//console.log(card)
-//console.log(card.children)
-//currentQuestion=0
-//timeleft
-//score
-//query select for button
+    //log(questions[0].answers[2])
+    //console.log(card)
+    //console.log(card.children)
 
 //start game
 //triggered event listener on start game button
+//function to present hidden elements
 startButton.addEventListener("click", function () {
    cardMain.classList.add("hide")
    card.classList.remove("hide")
    cardSection.classList.remove("hide")
 });
 //timer-setInterval-start timer-render to page
+//load first question
 startButton.addEventListener("click", function () {
     displayQuestion()
-    setInterval(function () {
+     var timeInterval = setInterval(function () {
         secondsLeft--;
         timerText.textContent = ("Seconds Left:" + secondsLeft);
+        if(secondsLeft===0 || index > 4){
+            clearInterval(timeInterval);
+            endGame();
+        }
     }, 1000);
 });
 
-//set-up first question
-//load question
-function displayQuestion (){
-    answerResponse.textContent=""
-    question.textContent = questions[index].question
-    buttonOne.textContent = questions[index].answers[0]
-    buttonTwo.textContent = questions[index].answers[1]
-    buttonThree.textContent = questions[index].answers[2]
-    buttonFour.textContent = questions[index].answers[3]
-}
+//render the question and multiple choices to the page
+
+function displayQuestion(){
+    answerResponse.textContent="";
+    question.textContent = questions[index].question;
+    buttonOne.textContent = questions[index].answers[0];
+    buttonTwo.textContent = questions[index].answers[1];
+    buttonThree.textContent = questions[index].answers[2];
+    buttonFour.textContent = questions[index].answers[3];
+};
+
+//Check answer
+//compare the choice with the correct answer 
+//act on right or wrong answer
+    //deduct 10 second for each incorrect answer
 function nextQuestion(event){
     if (event.target.textContent===questions[index].correct){
-        answerResponse.textContent="Correct!"
-    } else { 
-        answerResponse.textContent="Incorrect!"
+        answerResponse.textContent="Correct!";
+//score
+        score++
+        scoreReader.textContent= ("Score:" + score)
+    } else {
+        answerResponse.textContent="Incorrect!";
+        secondsLeft= secondsLeft-10;
     }
+    console.log(score)
+    console.log(secondsLeft)
+//Continue loading new questions
+//currentquestion++
     setTimeout(function(){
-        index++
-        displayQuestion()
-    },500)
+    index++;
+    displayQuestion()
+ },500)
 }
-
+//check which button user clicked
 buttonOne.addEventListener("click",nextQuestion)
 buttonTwo.addEventListener("click",nextQuestion)
 buttonThree.addEventListener("click",nextQuestion)
 buttonFour.addEventListener("click",nextQuestion)
 
-
-
-
-//question.textContent = JSONstringify(questions[0]);
-
-//Continue loading new questions
-//currentquestion++
-//render the question and multiple choices to the page
-//btn1.textContent=
-
-
-//Check answer
-//check which button user clicked
-//compare the choice with the correct answer 
-//act on right or wrong answer
-//score
-    //var score = 0;
-    //for (var i=0, i <questions.length; i++){
-    //var response = window.prompt(questions[i].prompt);
-    //if(response== question[i}.answer]){
-    //score++;
-    //answerResponse.textContent = ("Correct!")
-    //}else {
-    //answerResponse.textContent = ("Incorrect!")
-    //}
-    //}
-    //}
-//loadQuestion()
-
 //endGame
+
+
+function endGame(){
+
+}
+
 //called at end of timer or when current question> #of questions
+  //if (secondsLeft===0);
+//console.log("endGame");
+  //}
+
+//function stopTimer() {
+  //  if (secondsLeft<0 || questions>4)
 //cancel timer
+
 //prompt for user initials
 // function finalInitials () {
 //     localStorage.setItem("initials",(letters));
@@ -140,10 +141,10 @@ buttonFour.addEventListener("click",nextQuestion)
 //         storeletters();
 // })
 // };
-        //1. read initials and store in js variable
-            //2. check if null
-            //3. update js variable with our new score
-            //4. render to page
-            //5. save updated js variable to ls
-    //make highscore visible
+//1. read initials and store in js variable
+//2. check if null
+//3. update js variable with our new score
+//4. render to page
+//make highscore visible
     //prompt to play again
+    //quit
